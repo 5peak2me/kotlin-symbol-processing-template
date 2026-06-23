f611ae24693455d5de17215a87dd5e0381c82fc6
5peak2me <461260911@qq.com>
2026-06-23 19:33:24 +0800

    refactor(runtime): add empty constructor parameters to Template
    Allows defining parameters for the Template annotation in future tasks.

--- runtime/src/main/java/com/github/speak2me/ksp/template/Template.kt
+++ runtime/src/main/java/com/github/speak2me/ksp/template/Template.kt
@@ -3,4 +3,5 @@ package com.github.speak2me.ksp.template
 @MustBeDocumented
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.SOURCE)
-public annotation class Template
+public annotation class Template(
+)
